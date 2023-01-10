d62f0e1254c3cb89e587dc1c21633b4e439ba8b4
Kotha, Madhuri <madhuri.kotha@zemosolabs.com>
2023-01-10 16:08:21 +0530

    Update jar with different name

--- generate_tar_transform.sh
+++ generate_tar_transform.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-rm convert_hl7_to_fhir.tar.gz
+rm convert_hl7_to_fhir1.tar.gz
 mkdir temp
 cd temp
 cp ../executable.sh .
 cp ../convertHL7ToFHIR.js .
-tar -czvf ../convert_hl7_to_fhir.tar.gz .
+tar -czvf ../convert_hl7_to_fhir1.tar.gz .
 cd ..
 rm -rf temp
