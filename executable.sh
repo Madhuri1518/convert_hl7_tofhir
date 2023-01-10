f00918cfd20751f761db92be8738a67330848bf3
Kotha, Madhuri <madhuri.kotha@zemosolabs.com>
2023-01-10 16:05:22 +0530

    Update node location in executable.sh

--- executable.sh
+++ executable.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/.nvm/versions/node/v16.2.0/bin/node $( dirname -- "$0"; )/convertHL7ToFHIR.js "$@"
+/home/ec2-user/.nvm/versions/node/v16.2.0/bin/node $( dirname -- "$0"; )/convertHL7ToFHIR.js "$@"
